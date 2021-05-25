366a065637faecbf611d3cd29b83234ffd930d13
MatteoM2000 <s1094605@studenti.univpm.it>
2021-05-25 21:53:31 +0200

    Definizione home

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
-# This is a sample Python script.
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
 
-# Press Maiusc+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from home.view.VistaHome import Ui_HOME
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# prova commit
-# prova 2
-# prova Ebram
+    app = QApplication(sys.argv)
+    HOME = QWidget()
+    vista_home = Ui_HOME()
+    vista_home.setupUi(HOME)
+    HOME.show()
+    sys.exit(app.exec())
