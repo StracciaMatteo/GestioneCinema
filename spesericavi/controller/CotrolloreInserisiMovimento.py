97de759de945a948ee6ade82f06d45139ff84f1a
StracciaMatteo <S1096089@studenti.univpm.it>
2021-05-28 18:38:11 +0200

    Inserimento ControlloreINserisciMovimento

---
+++ spesericavi/controller/CotrolloreInserisiMovimento.py
@@ -0,0 +1,7 @@
+from PyQt5.QtWidgets import QLineEdit, QMessageBox, QWidget
+
+
+class ControlloreINserisciMovimento():
+
+    def __init__(self):
+        super(ControlloreINserisciMovimento, self).__init__()
