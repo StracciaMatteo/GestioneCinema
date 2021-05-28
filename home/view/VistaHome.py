7d0be0dc03bf4e54aaa1e66df07e7a824193a512
MatteoM2000 <s1094605@studenti.univpm.it>
2021-05-28 15:02:04 +0200

    Aggiunto tasto logout (non funzionante)

--- home/view/VistaHome.py
+++ home/view/VistaHome.py
@@ -3,8 +3,15 @@ from PyQt5.QtWidgets import QWidget
 
 
 class VistaHome(QWidget):
+    # il costruttore deve ricevere un oggetto di tipo dipendente per poter stampare il nome dell'utente loggato
     def __init__(self):
         super(VistaHome, self).__init__()
         self.vista = uic.loadUi("home/view/HOME.ui", self)
-        # comando per mostrare o nascondere box dipendenti
+        self.vista.label_login_session.setText("Login effettuato da: PROVA")    # MODIFICARE
+        # comando per mostrare o nascondere box gestione dipendenti
         # self.vista.box_dipendenti.hide()
+
+        self.vista.btn_torna.clicked.connect(self.logout)
+
+    def logout(self):
+        self.vista.close()
