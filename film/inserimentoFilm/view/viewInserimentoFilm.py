65441178495c0bd57d05103c301ba8ac6f69c0cf
MatteoM2000 <s1094605@studenti.univpm.it>
2021-05-31 21:41:09 +0200

    Implementati pulsanti e widget inserimento film

--- film/inserimentoFilm/view/viewInserimentoFilm.py
+++ film/inserimentoFilm/view/viewInserimentoFilm.py
@@ -1,5 +1,5 @@
 from PyQt5 import uic
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QWidget, QTableWidget, QPushButton, QTableWidgetItem
 
 
 class viewInserimentoFilm(QWidget):
@@ -7,3 +7,29 @@ class viewInserimentoFilm(QWidget):
         super(viewInserimentoFilm, self).__init__()
         self.widget = widget
         self.vista = uic.loadUi("film/inserimentoFilm/view/InserisciFilm.ui", self)
+
+        self.vista.btn_dialog.accepted.connect(self.save)
+        self.vista.btn_dialog.rejected.connect(self.go_back)
+
+        # Interazione con calendario
+        self.vista.calendar.clicked.connect(self.get_data)
+
+        # Interazioni con tabella
+        self.vista.table_programmazione.setEditTriggers(QTableWidget.NoEditTriggers)
+        self.vista.table_programmazione.doubleClicked.connect(self.assegna_data)
+
+    def get_data(self):
+        data = self.vista.calendar.selectedDate()
+        self.vista.label_data.setText(data.toString('dddd, d MMMM yyyy'))
+
+    def assegna_data(self, item):
+        self.vista.table_programmazione.setItem(item.row(), item.column(), QTableWidgetItem(str(self.vista.filmName.text())))
+
+    def save(self):
+        # aggiungere istruzioni per salvare film e poi chiude il widget
+        self.widget.setCurrentIndex(self.widget.currentIndex() - 1)
+        self.widget.removeWidget(self.vista)
+
+    def go_back(self):
+        self.widget.setCurrentIndex(self.widget.currentIndex() - 1)
+        self.widget.removeWidget(self.vista)
--- film/visualizzaProgrammazione/view/viewProgrammazione.py
+++ film/visualizzaProgrammazione/view/viewProgrammazione.py
@@ -7,3 +7,9 @@ class viewProgrammazione(QWidget):
         super(viewProgrammazione, self).__init__()
         self.widget = widget
         self.vista = uic.loadUi("film/visualizzaProgrammazione/view/ProgrammazioneSpettacoli.ui", self)
+
+        self.btn_torna.clicked.connect(self.go_back)
+
+    def go_back(self):
+        self.widget.setCurrentIndex(self.widget.currentIndex() - 1)
+        self.widget.removeWidget(self.vista)
