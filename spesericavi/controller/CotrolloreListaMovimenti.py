2503062cced4c8237bc1308e5e0d416403d36021
StracciaMatteo <S1096089@studenti.univpm.it>
2021-06-21 17:19:28 +0200

    Inserimento inizio Controller e Modello della Lista Movimento

--- spesericavi/controller/CotrolloreListaMovimenti.py
+++ spesericavi/controller/CotrolloreListaMovimenti.py
@@ -1,2 +1,12 @@
+from spesericavi.model.modelloListaMovimenti import modelloListaMoviemnti
+
+
+class ControllerListaMovimento():
+    def __init__(self):
+        super(ControllerListaMovimento,self).__init__()
+        self.modelloLista = modelloListaMoviemnti()
+
+    def get_voce(self,modello):
+        self.modelloLista.get_voce(modello)
 
 
