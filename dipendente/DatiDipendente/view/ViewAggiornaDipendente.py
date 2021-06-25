a66ae61fad43f238802bc73fb5608be965187bc8
Ebram-Rezkalla <eeebram@gmail.com>
2021-06-25 22:21:02 +0200

    implementazione classe Aggiorna dipendente

--- dipendente/DatiDipendente/view/ViewAggiornaDipendente.py
+++ dipendente/DatiDipendente/view/ViewAggiornaDipendente.py
@@ -1,4 +1,5 @@
-from PyQt5 import uic
+from PyQt5 import uic,QtCore, QtWidgets
+from PyQt5.QtCore import QDate
 from PyQt5.QtWidgets import QWidget
 
 
@@ -11,7 +12,9 @@ class ViewAggiornaDipendente(QWidget):
         self.btn_torna.clicked.connect(self.go_back)
         self.btn_Home.clicked.connect(self.go_home)
         self.visualizza_dipendente(dipendente)
-
+        self.ferie_on.clicked.connect(self.enable_ferie)
+        self.vista_aggiorna_dipendente.Ferie_dal.setDisabled(True)
+        self.vista_aggiorna_dipendente.Ferie_al_1.setDisabled(True)
     def go_back(self):
         self.widget.setCurrentIndex(self.widget.currentIndex() - 1)
         self.widget.removeWidget(self.vista_aggiorna_dipendente)
@@ -24,14 +27,30 @@ class ViewAggiornaDipendente(QWidget):
         self.vista_aggiorna_dipendente.Nome.setText(dipendente.nome)
         self.vista_aggiorna_dipendente.Cognome.setText(dipendente.cognome)
         self.vista_aggiorna_dipendente.Sesso.setCurrentText(dipendente.sesso)
-        #self.vista_aggiorna_dipendente.data_di_nascita.setText(dipendente.data_nascita)
+        self.vista_aggiorna_dipendente.Data_di_nascita.setDate(QtCore.QDate.fromString(dipendente.data_nascita,"dd/MM/yyyy"))
         self.vista_aggiorna_dipendente.Luogo_di_nascita.setText(dipendente.luogo_nascita)
-        #self.vista_aggiorna_dipendente.mansione.setText(dipendente.mansione)
+        self.vista_aggiorna_dipendente.Mansione.setCurrentText(dipendente.mansione)
         self.vista_aggiorna_dipendente.Codice_fiscale.setText(dipendente.cf)
         self.vista_aggiorna_dipendente.Stipendio.setText(dipendente.stipendio)
         self.vista_aggiorna_dipendente.ID.setText(dipendente.id)
-        #self.vista_aggiorna_dipendente.turn_lavoro.setText(dipendente.turno_lavoro)
-        #self.vista_aggiorna_dipendente.giorno_libero.setText(dipendente.giorno_libero)
-        #self.vista_aggiorna_dipendente.ferie_dal.setText(dipendente.ferie_dal)
-        #self.vista_aggiorna_dipendente.ferie_al.setText(dipendente.ferie_al)
+        self.vista_aggiorna_dipendente.Turno_di_lavoro.setCurrentText(dipendente.turno_lavoro)
+        self.vista_aggiorna_dipendente.Giorno_libero_1.setCurrentText(dipendente.giorno_libero.split()[0])
+        self.vista_aggiorna_dipendente.Giorno_libero_2.setCurrentText(dipendente.giorno_libero.split()[1])
+        self.leggi_ferie(dipendente.ferie_dal,dipendente.ferie_al)
         self.vista_aggiorna_dipendente.Commento.setText(dipendente.commento)
+
+
+    def leggi_ferie(self,ferie_dal,ferie_al):
+        if ferie_dal=="null" and ferie_al=="null":
+            self.vista_aggiorna_dipendente.Ferie_dal.setDate(QDate.currentDate())
+            self.vista_aggiorna_dipendente.Ferie_al_1.setDate(QDate.currentDate())
+        else:
+            self.vista_aggiorna_dipendente.Ferie_dal.setDate(QtCore.QDate.fromString(ferie_dal, "dd/MM/yyyy"))
+            self.vista_aggiorna_dipendente.Ferie_al_1.setDate(QtCore.QDate.fromString(ferie_al, "dd/MM/yyyy"))
+    def enable_ferie(self):
+       if self.vista_aggiorna_dipendente.ferie_on.isChecked():
+           self.vista_aggiorna_dipendente.Ferie_dal.setDisabled(False)
+           self.vista_aggiorna_dipendente.Ferie_al_1.setDisabled(False)
+       else:
+           self.vista_aggiorna_dipendente.Ferie_dal.setDisabled(True)
+           self.vista_aggiorna_dipendente.Ferie_al_1.setDisabled(True)
