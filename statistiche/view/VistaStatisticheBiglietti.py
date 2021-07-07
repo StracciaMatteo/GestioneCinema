4d93907397d19f85c54c9380269fcc484c2c8502
StracciaMatteo <S1096089@studenti.univpm.it>
2021-07-07 19:54:46 +0200

    Modifica e fine implementazione Statistiche Biglietti

--- statistiche/view/VistaStatisticheBiglietti.py
+++ statistiche/view/VistaStatisticheBiglietti.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+import numpy
+
 from PyQt5 import uic
 from PyQt5.QtWidgets import QWidget
 from listaFilm.controller.controllerListaFilm import controllerListaFilm
@@ -15,7 +17,7 @@ class VistaStatisticheBiglietti(QWidget):
 
         self.btn_tornaIndietroSB.clicked.connect(self.go_back)
         self.btn_GeneraDiagramma.clicked.connect(self.genera_stat)
-        print(self.controller.get_vendite_giornaliere())
+
 
 
     # Funizone che permette di tornare indietro con il tasto "<-" all'interno della vista
@@ -23,29 +25,15 @@ class VistaStatisticheBiglietti(QWidget):
         self.widget.setCurrentIndex(self.widget.currentIndex() - 1)
         self.widget.removeWidget(self.vista)
 
-    # Questa funzione riceve dal controller della lista fil ìm la somma di biglietti venduti per fascia oraria e ne crea un
-    # istogramma dividendo le quantità dei biglietti venduti per le varie fascie orarie degli spettacoli
+    # Questa funzione riceve dal controller della lista film la somma di biglietti venduti per fascia oraria e ne crea un
+    # istogramma mostrando a schermo gli istogrammi per fascia oraria,che indicano il numero di biglietti venduti
     def genera_stat(self):
-        x=[16,17,16,16,16,16,18,21,21,21,21,21,22,15,15,15,18,18,19,19,20,21,25,25,25,25,25,25,25,25,25,25]
-        #BINS= ovvero larghezza delle "colonne" dell'istogramma
-        number_of_bins=[15,18,21,24,27]
-        plt.figure(1)
-        names= ["         Spet.1","         Spet.2","         Spet.3","         Spet.4",""]
-        n, bins, patches = plt.hist(x,bins= number_of_bins, edgecolor= "black",color="y")
-        plt.title("STATISTICHE BIGLIETTI VENDUTI")
-        ax= plt.subplot(111)
-        ax.set_xticks(number_of_bins)
-        ax.set_xticklabels(names,rotation=0, rotation_mode="anchor", va="top",ha="left")
-        patches[0].set_fc("c")
-        patches[1].set_fc("y")
-        patches[2].set_fc("r")
-        patches[3].set_fc("m")
-        #patches[4].set_fc("b")
-        #patches[5].set_fc("w")
-        plt.xlabel("Spettacoli")
-        plt.ylabel("Numero biglietti venduti")
-        plt.legend(patches,names)
-        plt.tight_layout()
+        x= self.controller.get_vendite_giornaliere()
+        label=["15:00","18:00","21:00","00:00"]
+        fig = plt.figure(figsize=(8, 5))
+        plt.bar(label, x, color=['maroon', 'red', 'orange', 'yellow'],width=0.5,edgecolor="black")
+        plt.xlabel("Orari degli spettacoli")
+        plt.ylabel("Biglietti venduti")
+        plt.title("Statistiche sui biglietti venduti")
         plt.show()
-
-
+        print(x)
